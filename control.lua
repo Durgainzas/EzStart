8a3d94e452c78843ce539e8ec5cf5595f2367a8c
Sebastian Wallo <sebastian.wallo@konicaminolta.cz>
2018-03-29 21:04:58 +0200

    [change] Better script

--- control.lua
+++ control.lua
@@ -1,22 +1,60 @@
---data.lua
-script.on_event(defines.events.on_player_created, function(event)
-    local player = game.players[event.player_index]
-    player.insert{name="iron-plate", count=8}
-    player.insert{name="pistol", count=1}
-    player.insert{name="firearm-magazine", count=10}
-    player.insert{name="burner-mining-drill", count = 1}
-    player.insert{name="stone-furnace", count = 1}
-    player.force.chart(player.surface, {{player.position.x - 200, player.position.y - 200}, {player.position.x + 200, player.position.y + 200}})
-    if (#game.players <= 1) then
-        game.show_message_dialog{text = {"msg-intro"}}
-    else
-        player.print({"msg-intro"})
-    end
-    silo_script.gui_init(player)
-end)
+script.on_event(defines.events.on_console_command("test"), function(event)
+	local player = game.players[event.player_index]
+	
+	if defines.controllers.god == player.controller_type then
+		return
+	end
+	if defines.controllers.ghost == player.controller_type then
+		return
+	end
 
-script.on_event(defines.events.on_player_respawned, function(event)
-    local player = game.players[event.player_index]
-    player.insert{name="pistol", count=1}
-    player.insert{name="firearm-magazine", count=10}
+	local items
+	local armor
+
+	items = {
+		{"modular-armor", 1},
+        {"construction-robot", 20},
+        {"transport-belt", 800},
+        {"inserter", 200},
+        {"underground-belt", 154},
+        {"long-handed-inserter", 100},
+        {"stone-furnace", 100},
+        {"small-lamp", 20},
+        {"small-electric-pole", 98},
+        {"splitter", 16},
+        {"fast-inserter", 8},
+        {"iron-chest", 4},
+        {"iron-plate", 100},
+        {"copper-plate", 100},
+	}
+	armor = {
+		{"personal-roboport-equipment"},
+		{"personal-roboport-equipment"},
+		{"battery-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+		{"solar-panel-equipment"},
+	}
+
+	for i, v in pairs(items) do
+		player.insert{name = v[1], count = v[2]}
+	end
+
+	local grid = player.get_inventory(defines.inventory.player_armor)[1].grid
+	for  i, v in pairs(armor) do
+		grid.put({name = v[1]})
+	end
+
 end)
