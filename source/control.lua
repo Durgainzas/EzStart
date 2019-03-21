ba2c722e81c92cb5fd3664b01751ed19caf5cc1b
Sebastian Wallo <sebastian.wallo@konicaminolta.cz>
2019-03-21 02:05:00 +0100

    Changed script from using on_event to add_command

--- source/control.lua
+++ source/control.lua
@@ -1,58 +1,50 @@
-script.on_event(defines.events.on_console_command, function(event)
-    local player = game.players[event.player_index]
+function add_test_command () 
+    commands.add_command("test", "test-cmd", function(event)
+        local player = game.players[event.player_index]
+        local items
+        local armor
 
-    if event.command ~= "test" then
-        return
-    end
-
-    if defines.controllers.god == player.controller_type then
-        return
-    end
-    if defines.controllers.ghost == player.controller_type then
-        return
-    end
-
-    local items
-    local armor
-
-    items = {
-        {"power-armor-mk2", 1},
-        {"construction-robot", 50},
-        {"fast-transport-belt", 800},
-        {"fast-inserter", 200},
-        {"fast- underground-belt", 154},
-        {"long-handed-inserter", 100},
-        {"steel-furnace", 100},
-        {"small-electric-pole", 98},
-        {"fast-splitter", 16},
-        {"iron-chest", 4},
-        {"lamp", 4},
-        {"iron-plate", 100},
-        {"copper-plate", 100},
-    }
-    armor = {
-        {"personal-roboport-mk2-equipment"},
-        {"personal-roboport-mk2-equipment"},
-        {"battery-mk2-equipment"},
-        {"battery-mk2-equipment"},
-        {"fusion-reactor-equipment"},
-        {"fusion-reactor-equipment"},
-        {"fusion-reactor-equipment"},
-        {"fusion-reactor-equipment"},
-        {"exoskeleton-equipment"},
-        {"energy-shield-mk2-equipment"},
-        {"energy-shield-mk2-equipment"},
-        {"personal-laser-defense-equipment"},
-        {"night-vision-equipment"}
-    }
+        items = {
+            {"power-armor-mk2", 1},
+            {"construction-robot", 50},
+            {"fast-transport-belt", 800},
+            {"fast-inserter", 200},
+            {"fast- underground-belt", 154},
+            {"long-handed-inserter", 100},
+            {"steel-furnace", 100},
+            {"small-electric-pole", 98},
+            {"fast-splitter", 16},
+            {"iron-chest", 4},
+            {"lamp", 4},
+            {"iron-plate", 100},
+            {"copper-plate", 100},
+        }
+        armor = {
+            {"personal-roboport-mk2-equipment"},
+            {"personal-roboport-mk2-equipment"},
+            {"battery-mk2-equipment"},
+            {"battery-mk2-equipment"},
+            {"fusion-reactor-equipment"},
+            {"fusion-reactor-equipment"},
+            {"fusion-reactor-equipment"},
+            {"fusion-reactor-equipment"},
+            {"exoskeleton-equipment"},
+            {"energy-shield-mk2-equipment"},
+            {"energy-shield-mk2-equipment"},
+            {"personal-laser-defense-equipment"},
+            {"night-vision-equipment"}
+        }
 
         for i, v in pairs(items) do
             player.insert{name = v[1], count = v[2]}
         end
-    
+
         local grid = player.get_inventory(defines.inventory.player_armor)[1].grid
         for  i, v in pairs(armor) do
             grid.put({name = v[1]})
         end
+    end)
+end
 
-end)
+script.on_init(add_test_command)
+script.on_load(add_test_command)
